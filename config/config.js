0c0764bfd830393221118af884bc0ab94ea66c5e
Suborno22 <subornodas251@gmail.com>
2025-08-12 16:29:57 +0530

    Initial commit with all project files

---
+++ config/config.js
@@ -0,0 +1,19 @@
+const Dotenv = require("dotenv")
+
+Dotenv.config()
+
+const config = {
+    "dialect": process.env.DB_DIALECT ||"mysql",
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    // "database": process.env.DB_DATABASE,
+}
+
+module.exports = {
+    url: process.env.DB_CONNECTION_URL,
+    development: {...config, database: process.env.DB_DEVELOPMENT},
+    test: { ...config, database: process.env.DB_TEST_NAME },
+    production: { ...config, database: process.env.DB_PROD_NAME },
+}
---
+++ migrations/20250812104941-create-note.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Notes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      content: {
+        type: Sequelize.TEXT
+      },
+      category: {
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      reminderTime: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Notes');
+  }
+};
